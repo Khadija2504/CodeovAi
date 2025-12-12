9f8a09105b99bbc61be8c68ffcf4d1be1327fa41
abdelllatif <haissouneabdellatif749@gmail.com>
2025-12-12 11:38:03 +0100

    configuer le ficher dependances via pom.xml file

---
+++ Projet de fin d’année JobStream/JobStream/src/test/java/com/Job/JobStream/JobStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Job.JobStream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
