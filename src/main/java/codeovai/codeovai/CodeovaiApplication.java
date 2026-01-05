a7c4564d284615b582efa8d3cc0916d73e395bc4
khadijaourraiss <k.ourraiss@zf-secure.com>
2026-01-05 02:15:32 +0100

    create the codeovAi backend with basic configuration

---
+++ src/main/java/codeovai/codeovai/CodeovaiApplication.java
@@ -0,0 +1,13 @@
+package codeovai.codeovai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeovaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeovaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codeovai/codeovai/CodeovaiApplicationTests.java
@@ -0,0 +1,13 @@
+package codeovai.codeovai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeovaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
